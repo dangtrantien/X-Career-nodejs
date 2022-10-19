d43bf04b500b0a21c93310d5fec9963bf9e8cc18
doxuandungx10 <52046541+doxuandungx10@users.noreply.github.com>
2022-10-19 21:50:43 +0700

    initial front-end

---
+++ client/src/app/pages/main-view/main-view.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/models/board.model';
+import { IBoard } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/column.model';
+
+@Component({
+  selector: 'app-main-view',
+  templateUrl: './main-view.component.html',
+  styleUrls: ['./main-view.component.scss']
+})
+export class MainViewComponent implements OnInit {
+  isEditing: boolean = false;
+  constructor() { }
+
+  board: Board = new Board('Test Board', [
+    new Column('Ideas', [
+      "Some random idea",
+      "This is another random idea",
+      "build an awesome application"
+    ]),
+    new Column('Research', [
+      "Lorem ipsum",
+      "foo",
+      "This was in the 'Research' column"
+    ]),
+    new Column('Todo', [
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep'
+    ]),
+    new Column('Done', [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog'
+    ])
+  ]);
+
+  board2: IBoard = {
+    name: "Test Board",
+    columns: [
+      {
+      name: "Ideas",
+      tasks: ["Some random idea",
+      "This is another random idea",
+      "build an awesome application"]
+      },
+      {
+      name: "Research",
+      tasks: ["Lorem ipsum",
+      "foo",
+      "This was in the 'Research' column"]
+      }
+    ]
+  }
+
+  ngOnInit() {
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+
+  method($event) {
+    this.isEditing = !this.isEditing;
+    console.log("abc");
+  }
+
+
+}
